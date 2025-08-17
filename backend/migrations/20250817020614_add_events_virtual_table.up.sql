957f6d92616c5215eaa91547ec2aa0385c05e2d8
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2025-08-17 15:14:48 +0500

    fts

--- backend/migrations/20250817020614_add_events_virtual_table.up.sql
+++ backend/migrations/20250817020614_add_events_virtual_table.up.sql
@@ -4,5 +4,6 @@ CREATE VIRTUAL TABLE "events_archive_fts" USING fts5 (
     description
 );
 
-INSERT INTO events_archive_fts (id, title, description)
-SELECT id, title, description FROM events_archive;
+-- Note: The following commented-out section is for populating the FTS table.
+-- INSERT INTO events_archive_fts (id, title, description)
+-- SELECT id, title, description FROM events_archive;
