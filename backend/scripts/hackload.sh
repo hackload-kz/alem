f78dce6dcb137886068a9c53bcf4e4c6f722f062
Alimukhamed Tlekbai <ali.tlekbai@gmail.com>
2025-08-24 00:57:30 +0500

    feat: add k6 tests

--- backend/scripts/hackload.sh
+++ backend/scripts/hackload.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 
-API_ADDR="https://alem.hub.hackload.kz"
-# API_ADDR="http://localhost:8080"
+# API_ADDR="https://alem.hub.hackload.kz"
+API_ADDR="http://localhost:8080"
 AUTH_HEADER="Authorization: Basic YXlzdWx0YW5fdGFsZ2F0XzFAZmVzdC50aXg6LzhlQyRBRD4="
 
 BOOKING_ID="1"
@@ -16,7 +16,7 @@ set -x
 # curl  -s -H "$AUTH_HEADER" "$API_ADDR/api/events"
 # curl  -s -H "$AUTH_HEADER" "$API_ADDR/api/bookings"
 # curl  -s -H "$AUTH_HEADER" -X POST -d '{"event_id": 1}' "$API_ADDR/api/bookings"
-curl  -s -H "$AUTH_HEADER" "$API_ADDR/api/seats?event_id=1"
+# curl  -s -H "$AUTH_HEADER" "$API_ADDR/api/seats?event_id=1"
 # curl  -s -vvv -H "$AUTH_HEADER" -X PATCH -d "{\"seat_id\":$SEAT_ID, \"booking_id\":$BOOKING_ID}" "$API_ADDR/api/seats/select"
 # curl  -s -vvv -H "$AUTH_HEADER" -X PATCH -d "{\"seat_id\":$SEAT_ID}" "$API_ADDR/api/seats/release"
 # curl  -s -vvv -H "$AUTH_HEADER" -X PATCH -d "{\"booking_id\":$BOOKING_ID}" "$API_ADDR/api/bookings/initiatePayment"
